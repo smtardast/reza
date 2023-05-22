09a72d44fafbd7289ec5d3c7f9c3d3c3d56bb4b7
smtardast <43958348+smtardast@users.noreply.github.com>
2023-05-22 22:05:48 +0000

    footer done

--- reza/src/components/footer.js
+++ reza/src/components/footer.js
@@ -1,33 +1,47 @@
 function Footer() {
-    return(
-<div className="row">
-    <div className="col-sm-4 footer1">
+  return (
+    <div className="row sticky-bottom p-1 pt-5 pb-5">
+      <div className="col-4 footer1 text-start">
         <div className="row">
-        <a href="#top" className="footert">Reza Tardast</a>
-    </div>
-    </div>
-    <div className="col-sm-4 footer2">
-        <div className="row">
-            <img src="" alt=""></img>
+          <a href="#top" className="footert">
+            Reza Tardast
+          </a>
         </div>
-    </div>
-    <div className="col-sm-4 footer3">
+      </div>
+      <div className="col-4 footer2 text-center">
         <div className="row">
-            <ul className="list-inline">
-                <li className="list-inline-item">
-                    <a href="http://" className="footersocial footersocial1"><img src="" alt="" srcset=""></img></a>
-                </li>
-                <li className="list-inline-item">
-                    <a href="http://" className="footersocial footersocial1"><img src="" alt="" srcset=""></img></a>
-                </li>
-                <li className="list-inline-item">
-                    <a href="http://" className="footersocial footersocial1"><img src="" alt="" srcset=""></img></a>
-                </li>
-              </ul>
+          <a href="#top">
+            <i className="bi bi-arrow-up"></i>
+          </a>
+          {/*  arrow alternatives       
+<i class="bi bi-chevron-up"></i>
+<i class="bi bi-chevron-compact-up"></i>
+ */}{" "}
         </div>
+      </div>
+      <div className="col-4 footer3 text-end">
+        <div className="row">
+          <ul className="list-inline ">
+            <li className="list-inline-item pl-5 pr-5">
+              <a href="http://" className="footersocial footersocial1">
+                <i className="bi bi-instagram"></i>
+              </a>
+            </li>
+            <li className="list-inline-item pl-5 pr-5">
+              <a href="http://" className="footersocial footersocial1">
+                <i className="bi bi-envelope"></i>
+              </a>
+            </li>
+            <li className="list-inline-item pl-5 pr-5">
+              <a href="http://" className="footersocial footersocial1">
+                <img src="" alt=""></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-</div>
-    );
+  );
 }
 
 export default Footer;
--- reza/src/components/section.js
+++ reza/src/components/section.js
@@ -4,14 +4,26 @@ import Footer from "./footer";
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Section() {
-    return (
-        <section>
-            <Nav/>
-            <h1>babe</h1>
-            <Footer/>
-        </section>
-            
-    );
+  return (
+    <section>
+      <Nav />
+      <h1>babe</h1>
+      <p>
+        Tu es une contamination Tu es un vent d’espoir Tu es une beauté Tu es
+        une liberté Dépasser les normes Dépasser tout Tu es une liberté C’est la
+        plus belle chose Le chien qui court Qui est lui malgré tout Tu es le
+        chien haletant Qui tourne dans la rue noire Le sol brille dans la nuit
+        J’ai de la chance De connaître la contamination De ton genre Qui brille
+        dans la nuit De ta liberté Vivante et jamais éteinte J’ai illuminé les
+        nuits De tous ceux qui savent Qu’au fond d’eux Brille le temps futur De
+        la liberté Et de la poésie De ce qu’on a de soi même Au fond de soi
+        c'est ce qu'iel m'a écrit, j'ai pleuré. iel m'a vu, je l'ai senti. je me
+        suis senti aimé malgré le bruit de bière, la sueur. je me demande si on
+        était supposé s'aimer en fait. Iel est parti.{" "}
+      </p>
+      <Footer />
+    </section>
+  );
 }
 
 export default Section;
